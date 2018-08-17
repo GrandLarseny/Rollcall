76949946eb38ec9e2a8263c99cb316c5826acfde
Daniel Larsen <daniel.larsen@bottlerocketstudios.com>
2018-08-17 16:25:42 -0500

    Print out firebase raw body

--- lita-rollcall.rb
+++ lita-rollcall.rb
@@ -83,7 +83,7 @@ module Lita
         standupPath = "standups/#{response.room.id}"
         
         rollcallResponse = firebase.get(standupPath, "orderBy=\"date\"&equalTo=\"#{Date.today.to_s}\"")
-        puts "DDL: Found standups #{rollcallResponse}"
+        puts "DDL: Found standups #{rollcallResponse.raw_body}"
 
         rollcallResponse.body.each do |key, value|
           puts "DDL: --- rollcall #{value}"
