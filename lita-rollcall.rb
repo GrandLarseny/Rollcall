f3dc7e56795b022b0c81b804547b91bd0188db65
Daniel Larsen <daniel.larsen@bottlerocketstudios.com>
2018-08-18 09:20:47 -0500

    Change copy for replying to status

--- lita-rollcall.rb
+++ lita-rollcall.rb
@@ -53,7 +53,7 @@ module Lita
         puts "DDL: Calling standup for today - #{today}"
         addStandup(response, today, yesterday, blockers)
 
-        response.reply("Cool beans, @#{response.user.mention_name}! Got your standup for the day.")
+        response.reply("Copy that, @#{response.user.mention_name}!")
       end
 
       def addStandup(response, today, yesterday, blockers)
