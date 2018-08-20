2f001df16f8e7b37cf0680e7d098fab5e0fcaea9
Daniel Larsen <daniel.larsen@bottlerocketstudios.com>
2018-08-20 11:16:50 -0500

    Help text

--- lita-rollcall.rb
+++ lita-rollcall.rb
@@ -14,7 +14,7 @@ module Lita
       route(/^what *it *do$/, :helpMe, command: true)
       route(/^halp$/, :helpMe, command: true)
 
-      route(/(t|today|y|yesterday|b|blocker) *[-:]\s*/i, :standup, command: true)
+      route(/(t|today|y|yesterday|b|blocker|blocked by) *[-:]\s*/i, :standup, command: true)
 
       route(/^callout/i, :replyRollcall, command: true)
       route(/^list/i, :replyRollcall, command: true)
@@ -27,6 +27,20 @@ module Lita
       def firebaseRef
         base_uri = 'https://br-rollcall.firebaseio.com/'
         firebase = Firebase::Client.new(base_uri)
+      end
+
+      def helpMe(response)
+        helpText = "Hello, @#{response.user.mention_name}! I'm Rollcall, a chatbot designed to help you keep track of the daily standup.
+        There are a handful of things you can do. You can add a standup status, remove the latest status, or list all the standups for day.
+          To add a status, just format it with today's status, yesterday's status, and any blockers if applicable. All three are optional.
+          For example, '@Rollcall Yesterday: Worked on the test scripts. Today: Testing out the capacitor. Blocker: Rain.'
+          
+          To remove a status, just tell me to remove the last status.
+          For example, '@Rollcall remove'
+          
+          And to display the statuses, just tell me to list them out.
+          For example, '@Rollcall list'"
+        response.reply(helpText)
       end
 
       def echo(response)
@@ -69,7 +83,6 @@ module Lita
         firebase.push("standups", { :user => response.user.mention_name, :room => response.room.id, :date => date, :today => today, :yesterday => yesterday, :blockers => blockers, :timestamp => Time.now.to_i })
       end
 
-
       def replyRollcall(response)
         rollcall = ""
 
@@ -96,31 +109,31 @@ module Lita
         if rollcall.empty?
           response.reply("_[tumbleweeds roll]_ Pretty empty around here. No one's reported in yet.")
         end
-    end
+      end
 
-    def removeLast(response)
-      standups = toadysStandups(response.room.id)
-      myStandups = standups.select { |key, standup| standup["user"] = response.user.mention_name }
-      lastStandup = myStandups.max_by { |k, standup| standup["timestamp"] }
-      puts "DDL: Removing standup #{lastStandup[0]}"
+      def removeLast(response)
+        standups = toadysStandups(response.room.id)
+        myStandups = standups.select { |key, standup| standup["user"] = response.user.mention_name }
+        lastStandup = myStandups.max_by { |k, standup| standup["timestamp"] }
+        puts "DDL: Removing standup #{lastStandup[0]}"
 
-      firebase = firebaseRef()
-      firebase.delete("standups/#{lastStandup[0]}")
+        firebase = firebaseRef()
+        firebase.delete("standups/#{lastStandup[0]}")
 
-      response.reply("Forget it ever happened, @#{response.user.mention_name}")
-    end
+        response.reply("Forget it ever happened, @#{response.user.mention_name}")
+      end
 
-    def toadysStandups(room)
-      firebase = firebaseRef()
-      rollcallResponse = firebase.get("standups", "orderBy=\"room\"&equalTo=\"#{room}\"")
-      puts "DDL: Found standups #{rollcallResponse.raw_body}"
+      def toadysStandups(room)
+        firebase = firebaseRef()
+        rollcallResponse = firebase.get("standups", "orderBy=\"room\"&equalTo=\"#{room}\"")
+        puts "DDL: Found standups #{rollcallResponse.raw_body}"
 
-      date = Date.today.to_s
+        date = Date.today.to_s
 
-      rollcallResponse.body.select { |key, standup| standup["date"] == date }
-    end
+        rollcallResponse.body.select { |key, standup| standup["date"] == date }
+      end
 
-    Lita.register_handler(self)
+      Lita.register_handler(self)
     end
   end
 end
